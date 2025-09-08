2c4c338538295d2d77bbe9c5551ea929a1b30c27
Yusuke Tanaka <wing0920@gmail.com>
2025-09-08 19:52:55 +0900

    chore: make now optional, defaulting to new Date

--- util.ts
+++ util.ts
@@ -2,7 +2,10 @@
  * Parse the Retry-After header as per spec:
  * https://datatracker.ietf.org/doc/html/rfc9110#section-10.2.3
  */
-export function parseRetryAfter(headers: Headers, now: Date): Date | null {
+export function parseRetryAfter(
+  headers: Headers,
+  now: Date = new Date(),
+): Date | null {
   const retryAfter = headers.get("Retry-After");
   if (!retryAfter) return null;
 
